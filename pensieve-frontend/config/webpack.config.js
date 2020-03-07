3d1320487e466e649a7063ff3770af0c6905c34c
JeonBH <pensieve92@gmail.com>
2020-03-07 18:26:00 +0900

    sass-loader 수정

--- pensieve-frontend/config/webpack.config.js
+++ pensieve-frontend/config/webpack.config.js
@@ -455,13 +455,19 @@ module.exports = function(webpackEnv) {
             {
               test: sassRegex,
               exclude: sassModuleRegex,
-              use: getStyleLoaders(
-                {
+              use: getStyleLoaders({
                   importLoaders: 3,
                   sourceMap: isEnvProduction && shouldUseSourceMap,
-                },
-                'sass-loader'
-              ),
+                }).concat({
+                  loader: require.resolve('sass-loader'),
+                  options: {
+                    prependData: `@import 'utils';`,
+                    sassOptions: {
+                      includePahts : [pahts.appSrc + '/styles'],
+                      sourceMap: isEnvProduction && shouldUseSourceMap,
+                    }
+                  }
+                }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
