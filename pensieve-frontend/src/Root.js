45b1deb5dbb6b8b57eb43ddeab51e8bea1ee5063
JeonBH <pensieve92@gmail.com>
2020-03-09 20:51:19 +0900

    Root에 store 지정

--- pensieve-frontend/src/Root.js
+++ pensieve-frontend/src/Root.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from 'components/App.js';                 
+import App from 'components/App.js';
+import { Provider } from 'react-redux';
+import configure from 'store/configure';
 
-class Root extends Component {
-    render() {
-        return (
+
+const store = configure();
+
+const Root = () =>  {   
+    return (
+        <Provider store={store}>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-        );
-    }
+        </Provider>
+    );   
 }
 
 export default Root;
