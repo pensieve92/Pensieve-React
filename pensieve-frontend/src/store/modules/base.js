03c73ea1860483659e163673adf55fff6d01bbe5
JeonBH <pensieve92@gmail.com>
2020-03-09 22:03:39 +0900

    ducks구조 만들기

---
+++ pensieve-frontend/src/store/modules/base.js
@@ -0,0 +1,18 @@
+import { createActions, handleActions } from 'redux-actions';
+
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+// action type
+
+
+// action creators
+
+
+// initial state
+const initialState = Map({})
+
+// reducer
+export default handleActions({
+    
+}, initialState)
