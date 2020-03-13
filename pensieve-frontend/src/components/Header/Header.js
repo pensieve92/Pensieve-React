e6a239e8d5861377f622a8353e3d16c08acc2750
JeonBH <pensieve92@gmail.com>
2020-03-13 22:28:10 +0900

    Header 배치완료

--- pensieve-frontend/src/components/Header/Header.js
+++ pensieve-frontend/src/components/Header/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import logo from 'img/logo/icon-BLUE6.png'
+import menu from 'img/menu.png'
 
 const cx = classNames.bind(styles);
 
 const Header = () => (
-  <div className={cx('Header')} >
-    Header
-  </div>
+  <header className={cx('Header')} >
+    <div className={cx('header-content')}>
+      <div className={cx('brand')}>
+        <div className={cx('menu')}>
+          <img className={cx('menu-icon')} src={menu} />
+        </div>
+        <div>
+          <img className={cx('logo')} src={logo}/>        
+          <Link to="/">Pensieve</Link>        
+        </div>        
+      </div>
+      <div className={cx('right')}></div>
+    </div>    
+  </header>
 );
 
 export default Header;
